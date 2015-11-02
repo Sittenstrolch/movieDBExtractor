a012f9d6a7c53bb6bc684970223a1408e1070c91
Markus Petrykowski <github@markus-petrykowski.de>
2015-11-02 12:03:01 +0100

    some error handling

--- extractActors.js
+++ extractActors.js
@@ -2,7 +2,7 @@ var db = require('./database.js'),
     q  = require('q'),
     request = require('request')
 
-var startId = 51850,
+var startId = 97827,
     api_key = "da7e24d8289e92514bc64236c97d2874",
     max_id = "1529791"
 
@@ -28,11 +28,16 @@ function catchActors(id){
                     currentId += 1
                     getForId(currentId)
                 }else if (err == "wait"){
+                    console.log("Wait triggered in id: " + currentId)
                 	setTimeout(function() {
                     	getForId(currentId)
-					}, 5000);
+					}, 3000);
                 }else{            
                     console.log(err)
+                    console.log("Error triggered in id: " + currentId)
+                    setTimeout(function() {
+                        getForId(currentId)
+                    }, 5000);
                 }
             })
     }
@@ -58,18 +63,18 @@ function getActorById(id){
 
     request(parameters, function (error, response, body) {
         if(body){
-	if(body.status && (body.status != 200 || body.status == 403)){
-            deferred.reject({
-                id: id,
-                error: "Person API call " + body.message
-            })
-            return
-        }
+            if(body.status && (body.status != 200 || body.status == 403)){
+                    deferred.reject({
+                        id: id,
+                        error: "Person API call " + body.message
+                    })
+                    return
+                }
 
-        deferred.resolve(body)
-	}else {
-		deferred.reject()
-	}
+                deferred.resolve(body)
+        }else {
+            deferred.reject()
+        }
     })
 
     return deferred.promise
@@ -90,18 +95,18 @@ function getActorsCredits(id){
 
     request(parameters, function (error, response, body) {
         if(body){
-	if(body.status && (body.status != 200 || body.status == 403)){
-            deferred.reject({
-                id: id,
-                error: "Credit API call " + body.message
-            })
-            return
-        }	
+            if(body.status && (body.status != 200 || body.status == 403)){
+                    deferred.reject({
+                        id: id,
+                        error: "Credit API call " + body.message
+                    })
+                    return
+                }
 
-        	deferred.resolve(body)
-	}else{
-		deferred.reject()
-	}
+                    deferred.resolve(body)
+        }else{
+                deferred.reject()
+        }
 
     })
 
@@ -129,26 +134,31 @@ function getDataForActor(id){
                 }
             }
             //Got data for Actor
-            var dbPromises = []
-            dbPromises.push(db.addActor(result[0]))
-
-
-            var casts = result[1].cast,
+            var dbPromises = [],
+                casts = result[1].cast,
                 crews = result[1].crew,
                 actorId = result[0].id
 
-            for(var i=0; i<casts.length; i++){
-                if(casts[i].media_type == "movie")
-                    dbPromises.push(db.addMovieEntry(actorId, casts[i].id, casts[i].character))
-                else
-                    dbPromises.push(db.addTvShowEntry(actorId, casts[i].id, casts[i].character))
-            }
+            if(crews && casts && actorId) {
 
-            for(var i=0; i<crews.length; i++){
-                if(crews[i].media_type == "movie")
-                    dbPromises.push(db.addMovieCrew(actorId, crews[i].id, crews[i].job))
-                else
-                    dbPromises.push(db.addTvShowCrew(actorId, crews[i].id, crews[i].job))
+                dbPromises.push(db.addActor(result[0]))
+
+                for (var i = 0; i < casts.length; i++) {
+                    if (casts[i].media_type == "movie")
+                        dbPromises.push(db.addMovieEntry(actorId, casts[i].id, casts[i].character))
+                    else
+                        dbPromises.push(db.addTvShowEntry(actorId, casts[i].id, casts[i].character))
+                }
+
+                for (var i = 0; i < crews.length; i++) {
+                    if (crews[i].media_type == "movie")
+                        dbPromises.push(db.addMovieCrew(actorId, crews[i].id, crews[i].job))
+                    else
+                        dbPromises.push(db.addTvShowCrew(actorId, crews[i].id, crews[i].job))
+                }
+            }else{
+                deferred.reject("wait")
+                return
             }
 
             q.all(dbPromises)
